68b564168175d99f831bbf33ba426339baaf28c5
Sirius-146 <ruan_faria@yahoo.com.br>
2023-11-19 22:45:11 -0300

    second step: handling exceptions and filtering data

--- main.py
+++ main.py
@@ -45,11 +45,49 @@ def search_mode(search_method):
   return search_method
 
 def get_results(name,tipo):
+  import requests
   url = f'https://www.googleapis.com/books/v1/volumes?q={tipo}{name}&printType=books&langRestrict=pt&maxResults=5'
   response = requests.get(url)
   data = response.json()
   return data
 
+def separate_book_info(data):
+  information = []
+  for i in range(len(data["items"])):
+    volumeInfo = data["items"][i]["volumeInfo"]
+    information.append(volumeInfo)
+  return information
+
+def colect_errors(received_data):
+  books = []
+  for book in range(len(received_data)):
+    try:
+      title = (received_data[book]["title"]).title()
+    except KeyError:
+      title = None
+    try:
+      authors = received_data[book]["authors"][0]
+    except KeyError:
+      authors = None
+    try:
+      publisher = received_data[book]["publisher"]
+    except KeyError:
+      publisher = None
+    try:
+      pages = received_data[book]["pageCount"]
+    except KeyError:
+      pages = None
+    try:
+      description = received_data[book]["description"]
+      description = '.\n'.join(description.split('. '))
+    except KeyError:
+      description = None
+    books.append([title, authors, publisher, pages, description])
+  return books
+
+def show_results(book):
+  for i in range(len(book)):
+    print(f'{book[i][0]} \nAutor: {book[i][1]}\t Editora: {book[i][2]}\t Páginas: {book[i][3]}\nDescrição: \n{book[i][4]}')
 
 
 def main():
